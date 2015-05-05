d17e036a8498c69493ce3e57d7296672642e0f03
Ahas <adnan.alsinan@gmail.com>
2015-05-05 06:53:02 +0300

    Just added the tokens :)

--- Tweettest.js
+++ Tweettest.js
@@ -21,29 +21,29 @@ classifier.learn('awful', 'negative');
 
 
 var client = new Twitter({
-  consumer_key: '',
-  consumer_secret: '',
-  access_token_key: '',
-  access_token_secret: ''
+  consumer_key: 'bwy2OwQPRJDcu95gJAsRY9l5S',
+  consumer_secret: 'XX2jb3jX97dSGPCRyZi8TVDKxLsShnbmrBbG3LqNmOaNcSet1Y',
+  access_token_key: '3182886140-UPKCn7tNY2gcxs5AvW7lBjRtDLCheOqkDtQSRus',
+  access_token_secret: 'LF5OxrMlmI5mtRdEcqP3MPSWbRBvD6vIgyNpY3Mp8t2uJ'
 });
 
 
 //var util = require('util');
 
-client.stream('statuses/filter', {track: 'love'}, function(stream){
+client.stream('statuses/filter', {track: 'batman'}, function(stream){
 
   stream.on('data', function(data){
-    //console.log(data.text);
-    //console.log(classifier.categorize(data.text));
-    //stream.destroy();
-    //process.exit(0);
+    console.log(data.text);
+    console.log(classifier.categorize(data.text));
+    stream.destroy();
+    process.exit(0);
 
   });
 
 });
 
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
-   console.log(tweets.text);
+client.get('search/tweets', {q: 'love'}, function(error, tweets, response){
+   console.log(tweets);
 });
 //how to use bayes and train it?
 //by taking a tweet then asking after every tweet (manually)
