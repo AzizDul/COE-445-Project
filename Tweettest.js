2fe36aa3641230d1c6290271c854d5a81185184d
AzizDul <zez1992@gmail.com>
2015-05-19 14:49:59 +0300

    Added twitter Account Keys + update tweettest.js file

--- Tweettest.js
+++ Tweettest.js
@@ -3,13 +3,20 @@ var Twitter = require('twitter');
 var bayes = require('./bayes');
 
 var classifier = bayes();
+var x = 3;
+var tweetPost = "set";
 
 //Positive stuff:
 
 classifier.learn('amazing, awesome movie!! Yeah!! Oh boy.', 'positive');
 classifier.learn('Sweet, awesome , this is incredibly, amazing, perfect, great!!', 'positive');
-classifier.learn('Batman ', 'positive');
-classifier.learn('Batman , good stuff ', 'positive');
+classifier.learn('fun ', 'positive');
+classifier.learn('great ', 'positive');
+classifier.learn('love it ', 'positive');
+classifier.learn('love ', 'positive');
+classifier.learn('I like it, good stuff ', 'positive');
+classifier.learn('happy ', 'positive');
+
 
 
 //Negative stuff:
@@ -21,30 +28,50 @@ classifier.learn('awful', 'negative');
 
 
 var client = new Twitter({
-  consumer_key: 'Is89BrPuvd6Cyq3vGecH9tW9I',
-  consumer_secret: 'hvyDUP5IwTgGV0pyJEkr44VySh75YlmvDyGDyqeaqcFyimD5bI',
-  access_token_key: '189987329-jcy45yA977pWc7ZoHQAnLXQXGXibAZweuvBmrrY3',
-  access_token_secret: 'DpCZROb8NlxurzMR2iOGwuYPUa2WbArE55m39VdCad5ba'
+  consumer_key: 'bwy2OwQPRJDcu95gJAsRY9l5S',
+  consumer_secret: 'XX2jb3jX97dSGPCRyZi8TVDKxLsShnbmrBbG3LqNmOaNcSet1Y',
+  access_token_key: '3182886140-UPKCn7tNY2gcxs5AvW7lBjRtDLCheOqkDtQSRus',
+  access_token_secret: 'LF5OxrMlmI5mtRdEcqP3MPSWbRBvD6vIgyNpY3Mp8t2uJ'
 });
 
 
 //var util = require('util');
 
+//client.get('search/tweets', {q: 'use'} , function(error, tweets, response){
+//   console.log(tweets);
+//   console.log(error);
+//});
+
 client.stream('statuses/filter', {track: 'love'}, function(stream){
 
   stream.on('data', function(data){
-    //console.log(data.text);
-    //console.log(classifier.categorize(data.text));
-    //stream.destroy();
-    //process.exit(0);
-
+    if ((data.user.lang == 'en') && (data.lang == 'en')){
+      console.log('new Message:');
+      console.log(data.text);
+      console.log(data.created_at);
+      //console.log(data.lang);
+      //console.log(data.user.lang);
+      tweetPost = classifier.categorize(data.text);
+      console.log(tweetPost);
+      //console.log(data);
+      x--;
+      tweetPost = x+'';
+      client.post('statuses/update', {status: " " + tweetPost }, function(error, tweet, response){
+        if (!error) {
+          console.log(tweet.text);
+        }
+      });
+    }
+    if (x == 0){
+      stream.destroy();
+      process.exit(0);
+    }
   });
 
 });
 
-client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
-   console.log(tweets.text);
-});
+
+
 //how to use bayes and train it?
 //by taking a tweet then asking after every tweet (manually)
 //when asking, the answer should + or -, then after that the bayer code should learn
