2258c4569f7834f8abc70f414e6a9273e6489b79
john-locke <adnan.alsinan@gmail.com>
2015-05-24 08:08:37 +0300

    added mongolab.js, Basic working website

--- mongolab.js
+++ mongolab.js
@@ -4,41 +4,41 @@
 
 var mongodb = require('mongodb');
 
-
 // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+
 var uri = 'mongodb://RateMyMovie:123456@ds031661.mongolab.com:31661/heroku_app36793566';
 //var uri ='mongodb://localhost:27017/myDB'
-var seedData = [
-    {
-        input: 'amazing, awesome movie!! Yeah!! Oh boy. 2',
-        class: 'positive'
-    }
-];
+
+var training;
 
 
 mongodb.MongoClient.connect(uri, function(err, db) {
     MAINDB=db;
     if (err) console.log('Error in Connecting to DB'); else console.log('Connected to DB');
 
-    var training = db.collection('training');
-
-
-
-
-    console.log('trying to add');
-
-    training.insert(seedData, function(err, result) {
-
-        if (err) throw err;
-
-    });
-
+     training = db.collection('training');
 
 });
 
-    /*
-     * First we'll add a few songs. Nothing is required to create the
-     * songs collection; it is created automatically when we insert.
-     */
+
+ exports.AddtoDB = function (req,res) {
+
+training.insert(req, function(err, result) {
+
+    if (err) throw err;
+
+});
+
+}
+
+exports.getTrainingSet = function(reg,res) {
+
+    training.find().toArray(function (err2, docs) {
+
+        if (err2) throw err2;
+        console.log(docs);
+        res(docs);
+    })
 
 
+}
